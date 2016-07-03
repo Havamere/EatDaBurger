7fff0d9e2e1adc2fdef5ca3daa1dfb0a52041b43
Kyle R <kyle.rinehart83@gmail.com>
2016-07-03 16:49:00 -0400

    have a webpage displaying, nothing else really working yet

--- server.js
+++ server.js
@@ -3,10 +3,33 @@ var express = require('express');
 var methodOverride = require('method-override');
 var bodyParser = require('body-parser');
 var app = express();
-var burger = require('/modules/burger.js');
+var exphbs = require('express-handlebars');
+var connection = require('./config/connection.js');
+var path = require('path');
+//var burger = require('./models/burgers.js');
+
+//Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static(__dirname + '/public'));
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static('app/public'));
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+// app.get('/', function(req,res) {
+//     connection.query('SELECT * FROM quotes;', function(err, data) {
+//       if (err) throw err;
+//       res.render('index', {quotes : data});
+//     });
+// });
 
 app.get('/', function(req,res) {
-      res.render('index', {quotes : data});
+	connection.query('SELECT * FROM burgers;', function(err, data) {
+    	if (err) throw err;
+    	res.render('index', {burgers : data});
+	});
+	//eatenBurgers(res.render('index', res.body));
 });
 
 var port = 3000;
